307a27c459f8229ab7c29256f607074f3c1d7b62
Nicholas <hawx523@gmail.com>
2020-04-07 19:58:22 -0500

    Removed AppliedRoute

--- src/Articles/Article.jsx
+++ src/Articles/Article.jsx
@@ -9,22 +9,29 @@ export class Article extends React.Component {
         article: {
             title: "",
             content: "",
+            date: "",
             author: {
                 id: "",
                 name: ""
-            }
+            },
+            category: ""
         }
     }
 
     render() {
         return (
             <div id="article" className="container bg-white">
-                <section id="article-title">
+                <section id="article-header">
+                    <h5 className="badge badge-info" id="article-category">{ this.state.article.category.toUpperCase() }</h5>
                     <h1>{ this.state.article.title }</h1>
                     <hr />
+                    <h5 id="article-date">{ this.state.article.date }</h5>
+                    <h4 id="article-author">By { this.state.article.author.name }</h4>
                 </section>
                 <section id="article-body">
-                    <p>{ this.state.article.content }</p>
+                    {
+                        this.state.article.content.split('\n').map((p, i) => <p>{ p }</p>)
+                    }
                 </section>
             </div>
         );
